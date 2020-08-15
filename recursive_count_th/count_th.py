d6e01bace77bd99c11a41cdfa6b9a605be770485
Samuel Rowan <samuel@samuelrowan.com>
2020-08-14 20:41:02 -0500

    MVP on Recursive Count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,14 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     
     # TBC
-    
-    pass
+    # This version is better but better doesn't matter at Lambda.
+    # x=word.split('th')
+    # y=len(x)-1
+    search= ['t', 'h']
+    #This is the inferior version Lambda wants:
+    if word == '':
+        return 0
+    if word[:2] == "th":
+        return 1 + count_th(word[1:])
+    else:
+        return count_th(word[1:])
